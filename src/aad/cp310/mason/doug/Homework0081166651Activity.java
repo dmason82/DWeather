6f6be46d4fb41f636fbead079016c2f837a5da0d
Doug Mason <d.mason82@gmail.com>
2012-04-10 22:44:14 -0700

    setup layout

---
+++ src/aad/cp310/mason/doug/Homework0081166651Activity.java
@@ -0,0 +1,13 @@
+package aad.cp310.mason.doug;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Homework0081166651Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
