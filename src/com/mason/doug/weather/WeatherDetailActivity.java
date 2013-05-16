06b1513d8d97e6c64785f151d7afa93eb3926b56
Doug Mason <d.mason82@gmail.com>
2013-05-16 06:46:18 -0700

    Added WU banner at the bottom of main window again. Need to work on getting about in place, and get content provider up and working.

---
+++ src/com/mason/doug/weather/WeatherDetailActivity.java
@@ -0,0 +1,13 @@
+package com.mason.doug.weather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by dougmason on 5/15/13.
+ */
+public class WeatherDetailActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
