b33b82ea52de4236791014ad23e270f4d273a27d
Doug Mason <d.mason82@gmail.com>
2013-05-23 21:38:15 -0700

    Starting to break out into fragment based UI

--- src/com/mason/doug/weather/WeatherActivity.java
+++ src/com/mason/doug/weather/WeatherActivity.java
@@ -5,12 +5,17 @@ import android.os.Bundle;
 import android.database.Cursor;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
+import android.support.v4.widget.SimpleCursorAdapter;
 import com.mason.doug.weather2.R;
 
 /**
  * Created by dougmason on 5/23/13.
  */
 public class WeatherActivity extends FragmentActivity implements LoaderManager.LoaderCallbacks<Cursor>{
+    private SimpleCursorAdapter adapter;
+    private static final String CURRENT_TAG = "current";
+    private static final String FORECAST_TAG = "forecast";
+    private static final String INPUT_TAG = "input";
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dweather);
--- src/com/mason/doug/weather/WeatherForecastFragment.java
+++ src/com/mason/doug/weather/WeatherForecastFragment.java
@@ -4,4 +4,5 @@ import android.support.v4.app.ListFragment;
  * Created by dougmason on 5/23/13.
  */
 public class WeatherForecastFragment extends ListFragment {
+
 }
