095c4f4b6697e37c7c982d6d14a6ea9a723e45ca
Doug Mason <d.mason82@gmail.com>
2013-05-25 22:52:51 -0700

    Needed to place worker task for fetching bitmaps asynchronously into a class file such that I can use it in my fragments.

---
+++ src/com/mason/doug/weather/BitmapWorkerTask.java
@@ -0,0 +1,53 @@
+package com.mason.doug.weather;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.ref.WeakReference;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Created by dougmason on 5/25/13.
+ */
+class BitmapWorkerTask extends AsyncTask<String,Void,Bitmap> {
+    private final WeakReference<ImageView> imageViewReference;
+    private String data = "";
+    public BitmapWorkerTask(ImageView image){
+        imageViewReference = new WeakReference<ImageView>(image);
+    }
+    @Override
+    protected Bitmap doInBackground(String... params) {
+        // TODO Auto-generated method stub
+        data = params[0];
+        URL toConnect;
+        try {
+            toConnect = new URL(data);
+            URLConnection connect = toConnect.openConnection();
+            connect.connect();
+            InputStream content = (InputStream)toConnect.getContent();
+            Bitmap map = BitmapFactory.decodeStream(content);
+            return map;
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
+    }
+    protected void onPostExecute(Bitmap bitmap){
+        if(imageViewReference !=null && bitmap !=null){
+            final ImageView imageView = imageViewReference.get();
+            if(imageView !=null){
+                imageView.setImageBitmap(bitmap);
+            }
+        }
+    }
+}
--- src/com/mason/doug/weather/WeatherForecastFragment.java
+++ src/com/mason/doug/weather/WeatherForecastFragment.java
@@ -1,15 +1,66 @@
 package com.mason.doug.weather;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 import com.mason.doug.weather2.R;
+
+import java.util.List;
 
 /**
  * Created by dougmason on 5/23/13.
  */
 public class WeatherForecastFragment extends ListFragment {
+
+    private class ForecastConditionsAdapter extends ArrayAdapter<ForecastWeather> {
+
+        public ForecastConditionsAdapter(Context context, int resource,
+                                         int textViewResourceId, List<ForecastWeather> objects) {
+            super(context, resource, textViewResourceId, objects);
+            // TODO Auto-generated constructor stub
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View row = convertView;
+            ForecastWeather forecast = this.getItem(position);
+            if(row == null){
+                LayoutInflater inflater = getActivity().getLayoutInflater();
+                row = inflater.inflate(R.layout.forecast_conditions, parent, false);
+                row.setTag("Forecast "+Integer.toString(position));
+            }
+            TextView forecastDay = (TextView)row.findViewById(R.id.forecastLabel);
+            TextView forecastHigh = (TextView)row.findViewById(R.id.forecastHighText);
+            TextView forecastLow = (TextView)row.findViewById(R.id.forecastLowText);
+            TextView forecastCondtion = (TextView)row.findViewById(R.id.forecastConditionText);
+            ImageView forecastImage = (ImageView)row.findViewById(R.id.forecastImage);
+
+            forecastDay.setText(forecast.getDay());
+            /*if(!inC.isChecked()){
+                forecastLow.setText("Low: "+ String.format("%.2f",forecast.getLowTemp())+"�F");
+                forecastHigh.setText("High: "+String.format("%.2f",forecast.getHighTemp())+"�F");
+            }*/
+            //Temporary until I figure out how to get Celsius temperature properly notified throughout the activity.
+            if(false){
+
+            }
+            else{
+                float lowTemp = Utilities.fToC(forecast.getLowTemp());
+                float highTemp = Utilities.fToC(forecast.getHighTemp());
+                forecastLow.setText("Low: "+ String.format("%.2f",lowTemp)+"�C");
+                forecastHigh.setText("High: "+String.format("%.2f",highTemp)+"�C");
+            }
+            forecastCondtion.setText(forecast.getCondition());
+            new BitmapWorkerTask(forecastImage).execute(forecast.getIcon());
+            return row;
+
+        }
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.weather_forecast,container,false);
